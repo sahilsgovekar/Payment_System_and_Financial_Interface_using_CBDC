27e3ef054cfff35af136ef7fd1705ddfbe7929cc
sahilsgovekar <sahilsgovekar@gmail.com>
2023-01-08 21:42:52 +0530

    Added few feilds in sign up form and modified name in front end and backend

--- server.py
+++ server.py
@@ -7,14 +7,18 @@ app = Flask(__name__)
 def login():
     if request.method == "POST":
         if request.form["submit"] == "log_in":
-            siname = request.form["siname"]
-            sipass = request.form["sipass"]
-            print(siname, sipass)
+            l_username = request.form["l_username"]
+            l_password = request.form["l_password"]
+            print(l_username, l_password)
         
         if request.form["submit"] == "Sign_up":
-            username = request.form["username"]
-            print(username)
-
+            s_username = request.form["s_username"]
+            s_fname = request.form["s_fname"]
+            s_lname = request.form["s_lname"]
+            s_phoneno = request.form["s_phoneno"]
+            s_email = request.form["s_email"]
+            s_password = request.form["s_password"]
+            print(s_username, s_fname, s_lname, s_phoneno, s_email, s_password)
 
     return render_template("loginorsignup.html")
 
